e143f2a8dc61e4a4929fd84b8ff1c30ca27eee59
Regin Calaguas <regin_calaguas@yahoo.com>
2023-03-26 21:54:44 -0700

    Add files via upload

---
+++ src/component/variable.js
@@ -0,0 +1,78 @@
+export const _buttonAbsolute = {
+    top: "460px",
+    position: "absolute",
+}
+
+export const _bsize = {
+    width:"165px", 
+    height:"55px",
+}
+
+export const _button = {
+    color: "white",
+    borderRadius: "5px",
+    boxShadow: "5px 5px 10px #888888",     
+    fontFamily: "Medium",
+    fontSize: "20px",
+}
+export const _b1 = {
+    marginRight: "13px", 
+    background: "#0F9D58",
+}
+export const _b2 = {
+    left: "175px",                           
+    background: "#DB4437",
+}
+
+
+export const titleFont = {                    //format of the 15  puzzle title
+    textAlign: "center",
+    fontFamily: "Rubik",
+    letterSpacing: ".1em", 
+    color: "#0F9D58"
+}
+
+export const cbutton = {          
+    height: "50px",
+    cursor: "pointer",        
+    fontSize:  "40px",
+    color: "red",
+    border: "none",
+    background: "none",
+}
+
+export const bright = {
+    textAlign: "right"
+}
+
+export const modalLayout = {      
+    zIndex: "1", /* Sit on top */
+    left: "0",
+    top: "0",        
+    width: "100%", /* Full width */
+    height: "100%", /* Full height */        
+    position: "fixed", /* Stay in place */
+    display: "flex",
+    justifyContent: "center",
+    alignItem: "center",
+    fontFamily: "Medium",     
+    color: "#28292d",
+    overflow: "auto", /* Enable scroll if needed */            
+    backgroundColor: "rgba(0,0,0,0.90)" /* Black w/ opacity */          
+}
+export const _box = {
+    borderRadius: "5px",
+    transform: "translateY(50%)",
+    background: "white",
+    textAlign: "center",
+    padding: "30px",
+    boxShadow: "5px 5px 10px #888888",
+    height: "350px",        
+}
+
+export const  _textbox = {
+    borderRadius: "2px",
+    marginRight: "15px",
+    // border: "none",
+    fontSize: "17px",
+}
