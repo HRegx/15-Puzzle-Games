c1ebdcd42982607176c42b7db07ed3c349547b83
Regin Calaguas <regin_calaguas@yahoo.com>
2023-04-06 21:36:39 -0700

    Add files via upload

---
+++ src/component/db.js
@@ -0,0 +1,44 @@
+
+const endpoint = 'https://ec2-54-157-167-21.compute-1.amazonaws.com:8080/';
+export default function DbCommand( { _command } ){
+
+}
+
+export function inject(topScore,getTime,f_sname){
+    fetch(endpoint + 'inject', { 
+        // fetch('http://localhost:3001/inject', { 
+        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        body: JSON.stringify({
+            f_score: topScore,
+            f_time: getTime,
+            f_screen_name: f_sname
+        }),
+    }).then(()=>{
+        console.log("success");
+    });
+}
+
+
+export async function getHighScore(){
+    try{
+        const response = await fetch(endpoint + 'highScore');
+        const data = await response.json();
+        return data;
+        // console.log(data[0].f_score);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+export async function getTopTen(){
+    try{
+        const response  = await fetch(endpoint + 'handpick');     
+        const data = await response.json();        
+        return data;        
+    } catch (error){
+        console.log(error);
+    }
+}
+
