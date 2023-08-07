70a8783e08ee2409d7755b1808fec3e98e78f443
Regin Calaguas <regin_calaguas@yahoo.com>
2023-08-06 20:18:25 -0700

    Update quote.js

--- src/component/quote.js
+++ src/component/quote.js
@@ -13,14 +13,19 @@ export default function Quote(props){
     left: '50%', // Place the text 50% from the left of the container.
     transform: 'translate(-50%, -50%)', // Center the text using translate.
     // fontFamily: "Typew",
-    fontSize: '64px',    
+    fontSize: '34px',
+    color: 'black',  
+    width: 600,  
   }
 
     return(
         <>        
             <div style={modalLayout}>
                 <div style={{..._boxQ, height: "650px", width: "1150px"}}>
-                    <p style={_quote}>Repetetion is the mother of skill.. Action is where all of your power is found.. knowledge is not power. EXECUTION is. </p>
+                    {/* <p style={_quote}>Repetetion is the mother of skill.. Action is where all of your power is found.. knowledge is not power. EXECUTION is. </p> */}
+
+                    <p style={_quote}>simplicity and humility, will bring you joy and happiness</p>
+                  
                     <img src={'../quotes.png'} alt="" style={_number} />
                     <h1>Random Quotes</h1>
                     <h1>15 Puzzle</h1>
