33000895806dcb74de99696ab7e200a21b924c3f
Regin Calaguas <regin_calaguas@yahoo.com>
2023-08-07 19:39:51 -0700

    Update customFont.js

--- src/component/customFont.js
+++ src/component/customFont.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useFonts } from 'expo-font';
+
+export default function PersonalFont({ myText, myFont, mySize, myColor, myWidth, myAlign})  {
+
+const [fontsLoaded] = useFonts({
+          "STAMPWRITERKIT": require("../assets/fonts/STAMPWRITER-KIT.ttf"),
+          "RubikVinyl-Regular": require("../assets/fonts/RubikVinyl-Regular.ttf"),
+          "Hacked-KerX": require("../assets/fonts/Hacked-KerX.ttf"),                                        
+});          
+
+if (!fontsLoaded) {
+          return null;
+}
+
+return (          
+          <View>
+                    <Text style={{ fontFamily: myFont, fontSize: mySize, color: myColor, textAlign: myAlign, width: myWidth}}> {myText} </Text>
+          </View>
+);
+}
 
